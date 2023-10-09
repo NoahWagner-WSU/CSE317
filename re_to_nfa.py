2c67f38bd3412d2aff27d630fed50aca48ec2a6c
Noah Wagner <wagnernoah@icloud.com>
2023-10-09 16:08:58 -0700

    finished

--- re_to_nfa.py
+++ re_to_nfa.py
@@ -8,18 +8,25 @@ class NFA:
 		self.accept_state = accept_state
 
 	#outputs NFA to stdout in structured way (like in specification files)
-	# def print():
+	def print(self):
+		print("Start: q" + str(self.start_state))
+		print("Accept: q" + str(self.accept_state))
+		for transition in self.transitions:
+			start = str(transition[0])
+			symbol = str(transition[1])
+			end = str(transition[2])
+			print("(q" + start + ", " + symbol + ") -> q" + end)
 
 	def union(self, NFA2, curr_state_id):
 		transition1 = [curr_state_id, "E", self.start_state]
 		transition2 = [curr_state_id, "E", NFA2.start_state]
 		transition3 = [self.accept_state, "E", curr_state_id + 1]
 		transition4 = [NFA2.accept_state, "E", curr_state_id + 1]
+		self.transitions.append(transition3)
 		self.transitions.extend(NFA2.transitions)
+		self.transitions.append(transition4)
 		self.transitions.append(transition1)
 		self.transitions.append(transition2)
-		self.transitions.append(transition3)
-		self.transitions.append(transition4)
 		self.accept_state = curr_state_id + 1
 		self.start_state = curr_state_id
 		return self
@@ -42,7 +49,7 @@ input_file = open(sys.argv[1], "r")
 letters = ["a", "b", "c", "d", "e"]
 
 for line in input_file:
-	regex = line
+	regex = line[:-1]
 	curr_state_id = 1
 	NFAs = []
 	for char in regex:
@@ -56,8 +63,8 @@ for line in input_file:
 				exit()
 			NFA1 = NFAs.pop()
 			NFA2 = NFAs.pop()
-			NFA1.union(NFA2, curr_state_id)
-			NFAs.append(NFA1)
+			NFA2.union(NFA1, curr_state_id)
+			NFAs.append(NFA2)
 			curr_state_id += 2
 		elif char == "&":
 			if len(NFAs) < 2:
@@ -75,14 +82,13 @@ for line in input_file:
 			NFA1.star(curr_state_id)
 			NFAs.append(NFA1)
 			curr_state_id += 1
-		elif char != "\n":
+		else:
 			print("Error: unrecognized character: " + char)
 			exit()
-	# print regex, start state, accept states, and NFA in specified format
+	if len(NFAs) != 1:
+		print("Error: Malformed Input")
+		exit()
 	final_NFA = NFAs.pop()
-	print("___TRANSITIONS___")
-	print(final_NFA.transitions)
-	print("___START_STATE___")
-	print(final_NFA.start_state)
-	print("___ACCEPT_STATES___")
-	print(final_NFA.accept_state)
+	print("RE: " + regex)
+	final_NFA.print()
+	print("\n", end="")
